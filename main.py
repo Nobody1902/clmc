a78dd108a4b1a492b496dc8f82897f2a3701b645
Nobody1902 <136335406+Nobody1902@users.noreply.github.com>
2025-04-07 15:51:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from config import DEFAULT_CONFIG
 import sys
 
 config = DEFAULT_CONFIG
+# Change these config options to download Minecraft for another operating system
 # config.platform = "windows-x64"
 # config.platform_clean = "windows"
 
 if len(sys.argv) > 1:
-    launcher.install_version(sys.argv[1])
-    launcher.launch(sys.argv[1])
-
-# forge.install_forge("1.21.4", "54.0.26")
-# forge.launch("1.21.4", "54.0.26")
+    launcher.install_version(sys.argv[1], config=config)
+    launcher.launch(sys.argv[1], config=config)
